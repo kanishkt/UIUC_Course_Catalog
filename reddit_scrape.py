6730a8589c81027015bd961ea9c16141d4064bb9
Kanishk Thareja <kanishkt@gmail.com>
2016-04-23 17:47:19 -0500

    Initial commit

---
+++ reddit_scrape.py
@@ -0,0 +1,25 @@
+import datetime
+import json
+import praw
+
+
+def RedditData():
+
+
+    query = "cs+374"
+    time = "year"
+
+    r = praw.Reddit(user_agent='my_cool_application')
+    submissions = r.search(query, subreddit="uiuc", sort=None, syntax=None, period=time)
+    subreddit_posts = list()
+    for x in submissions:
+       points = x.ups - x.downs
+       time = x.created
+       datetimes = datetime.datetime.fromtimestamp(time)
+       subreddit_posts.append({"subreddit": "r/UIUC", "url": x.url, "title": x.title, "datetime": str(datetimes), "comments": 10, "points": points})
+
+    data_str = json.dumps(subreddit_posts)
+    print data_str
+
+if __name__ == '__main__':
+    RedditData()
